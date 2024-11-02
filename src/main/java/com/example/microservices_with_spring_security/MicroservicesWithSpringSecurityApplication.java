706cf7fd08824942159dfae31a7e67070837e94a
LydiaHsieh <hch34242@gmail.com>
2024-11-02 16:49:04 +0800

    init

---
+++ src/main/java/com/example/microservices_with_spring_security/MicroservicesWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservices_with_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microservices_with_spring_security/MicroservicesWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices_with_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
